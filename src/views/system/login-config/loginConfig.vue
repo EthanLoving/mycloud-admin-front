596983804324f8956e6333375e4e38fec5030477
houqijun <349514537@qq.com>
2019-06-16 12:39:19 +0800

    新增登录配置

---
+++ src/views/system/login-config/loginConfig.vue
@@ -0,0 +1,56 @@
+<style scoped>
+  @import "_loginConfig.scss";
+</style>
+<template>
+  <div class=".loginConfig">
+    <Card :bordered="false">
+      <Row>
+        <Col span="12">
+        <Button type="primary" icon="ios-add" @click="handleSetBg()">设置背景图片</Button>
+        </Col>
+      </Row>
+    </Card>
+    <Modal
+      v-model="bgAddModal"
+      title="新增路由"
+      :closable="false"
+      :mask-closable="false"
+      :loading="true"
+      width="1000px"
+      @on-ok="submitUpload()"
+    >
+      <Upload
+        multiple
+        type="drag"
+        action="//jsonplaceholder.typicode.com/posts/"
+      >
+        <div style="padding: 20px 0">
+          <Icon type="ios-cloud-upload" size="52" style="color: #3399ff" />
+          <p>点击此处上传文件</p>
+        </div>
+      </Upload>
+    </Modal>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LoginConfig',
+  data() {
+    return {
+      bgAddModal: false
+    }
+  },
+  methods: {
+    handleSetBg() {
+      this.bgAddModal = true
+    },
+    submitUpload() {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
