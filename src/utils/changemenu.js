62db2b5af2ce6d86c8fe59cb4c05e23c9d67def5
houqj <houqj@glodon.com>
2019-11-22 16:31:50 +0800

    修改路由

--- src/utils/changemenu.js
+++ src/utils/changemenu.js
@@ -1,6 +1,5 @@
 import chartsRouter from '../router/modules/charts'
-import router from '../router'
 export function chartsRouters() {
-  router.push(chartsRouter);
-  console.log(router)
+  this.$router.addRoutes(chartsRouter);
+  console.log(this.$router)
 }
